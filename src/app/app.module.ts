0ccee908961b533e161136f52e887d090ffe5fd6
avj1402 <aj.avinav@gmail.com>
2023-10-28 07:31:18 -0700

    Added media query

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from "@angular/material/icon";
 import { HttpClientModule } from "@angular/common/http";
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
