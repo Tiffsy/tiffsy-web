42b264167e51cef58e7c82662baed5123715c8bb
aashita-a <aashitaag08@gmail.com>
2023-10-29 21:13:43 +0530

    Added today-menu and download tiffsy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from "@angular/material/icon";
-import { MatCardModule } from "@angular/material/card";
 import { HttpClientModule } from "@angular/common/http";
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
 import { ImageCardComponent } from './image-card/image-card.component';
+import { TodayMenuComponent } from './today-menu/today-menu.component';
+import { DownloadTiffsyComponent } from './download-tiffsy/download-tiffsy.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { ImageCardComponent } from './image-card/image-card.component';
     ToolbarComponent,
     DashboardComponent,
     HeaderComponent,
-    ImageCardComponent
+    ImageCardComponent,
+    TodayMenuComponent,
+    DownloadTiffsyComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +32,6 @@ import { ImageCardComponent } from './image-card/image-card.component';
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule,
     HttpClientModule
   ],
   providers: [],
