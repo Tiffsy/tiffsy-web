f748a5687512ec8a065749aed8a366f0702b1c65
Tanisha Agarwal <tanishaagarwal1121@gmail.com>
2023-12-06 00:35:54 +0530

    login-signup-final

--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-signup',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-signup.component.scss']
 })
 export class LoginSignupComponent {
-
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+    private router: Router
+  ){
+    this.matIconRegistry.addSvgIcon(
+      "google",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/google-icon.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "help",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/help-icon.svg")
+    );
+  }
+  
 }
